f3d6f335b201b2b295b3d8bf8498725966130d01
Cachumbin <simoncarreno2006@gmail.com>
2025-05-09 20:29:19 -0500

    feat: Add cors to api

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swaggerConfig');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -10,10 +11,29 @@ const app = express();
 app.use(express.text());
 app.use(express.json());
 
-// Swagger UI
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+        return res.status(401).json({ error: 'Authorization header missing or malformed' });
+    }
+
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [username, password] = credentials.split(':');
+
+    if (username === process.env.API_USER && password === process.env.API_PASSWORD) {
+        next();
+    } else {
+        res.status(403).json({ error: 'Invalid credentials' });
+    }
+});
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Mount Routes
 app.use('/sync', syncRoutes);
 
 module.exports = app;
