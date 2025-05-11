54afe26064401b52b4b0588a7f57bd265a4fa5d0
Cachumbin <simoncarreno2006@gmail.com>
2025-05-11 00:35:54 -0500

    feat: Add headers log for debugging

--- src/app.js
+++ src/app.js
@@ -15,6 +15,13 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
+    console.log("🛰️  Incoming Request:");
+    console.log("Headers:", req.headers);
+    console.log("Body:", req.body);
+    next();
+});
+
+app.use((req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith('Basic ')) {
@@ -31,6 +38,7 @@ app.use((req, res, next) => {
         res.status(403).json({ error: 'Invalid credentials' });
     }
 });
+
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
